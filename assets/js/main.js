754c33cd98dd960cde36d2255dd306f5001a22e5
ASUS <lekietpro123@gmail.com>
2025-09-16 15:08:14 +0700

    Build Header & Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,37 @@
 /*=============== SHOW MENU ===============*/
-
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+// Menu show
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+// Menu Hidden
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
-
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SWIPER HOME ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
